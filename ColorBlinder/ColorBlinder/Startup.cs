b3136a7ba797fe383b999b816fd670e32097843b
Stuart Branham <sbranham@godaddy.com>
2016-10-27 10:49:43 -0700

    initial commit

---
+++ ColorBlinder/ColorBlinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ColorBlinder.Startup))]
+namespace ColorBlinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
