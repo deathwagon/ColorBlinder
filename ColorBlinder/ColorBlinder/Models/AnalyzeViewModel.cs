28fc292953ac0563fa27fe60ef21fa024e30fd03
Raj Vontela <rvontela@godaddy.com>
2016-10-27 15:17:52 -0700

    side by side image updates

--- ColorBlinder/ColorBlinder/Models/AnalyzeViewModel.cs
+++ ColorBlinder/ColorBlinder/Models/AnalyzeViewModel.cs
@@ -2,7 +2,9 @@
 {
   public class AnalyzeViewModel
   {
+    public string FilterType { get; set; }
     public string OriginalImagePath { get; set; }
     public string FilterImagePath { get; set; }
+    public string AllFilterImagePaths { get; set; }
   }
 }
